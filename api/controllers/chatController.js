11a2daf15d0de22491e44fe534e5fd58943c8cb1
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-07-11 19:33:25 +0700

    tambahin password cuy

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -2,6 +2,15 @@ import { chatService } from "../service/chatService";
 
 export async function chatConteroller(req, reply) {
     const message = req.body.msg
-    const result = await chatService(message)
-    reply.send(result)
+    const password = req.body.password
+
+    if (password == process.env.PASSWORD) {
+        const result = await chatService(message)
+        reply.send(result)
+    } else {
+        reply.send({
+            status: false,
+            msg: 'Password salah!! coba tanya developernya 😉.'
+        })
+    }
 }
